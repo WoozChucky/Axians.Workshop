44f1d98caf50e600d25c9d4b3c95165b484536b8
Nuno Silva <nuno.levezinho@live.com.pt>
2025-05-09 02:04:36 +0100

    Added Chess agent endpoint

--- Axians.Workshop.IST/Program.cs
+++ Axians.Workshop.IST/Program.cs
@@ -67,6 +67,38 @@ app.MapPost("/chat", async (IChatClient chatClient, [FromBody] ChessChatMessage 
     return response.Text;
 });
 
+app.MapPost("/move", async (IChatClient chatClient, [FromBody] ChessMoveRequest request) =>
+{
+    var chatOptions = new ChatOptions() { ConversationId = request.SessionId };
+
+    var response = await chatClient.GetResponseAsync([
+        new ChatMessage(ChatRole.System, "You are a Chess AI agent playing against a human. " +
+                                         "The human is playing as white. You are playing as black. The user provides the FEN of the current board state and the possible moves."),
+        request,
+    ], chatOptions);
+
+    // Parse the response
+    var lines = response.Text.Split(["\n", Environment.NewLine], StringSplitOptions.RemoveEmptyEntries);
+    if (lines.Length == 0)
+    {
+        return new ChessMoveResponse(false, "Invalid response format", null);
+    }
+    var isValid = lines[0].Trim().Equals("True", StringComparison.OrdinalIgnoreCase);
+
+    if (isValid)
+    {
+        var move = lines.Length > 1 ? lines[1].Trim() : null;
+        if (move is not null && !request.PossibleMoves.Contains(move))
+        {
+
+        }
+        return new ChessMoveResponse(isValid, null, move);
+    }
+
+    var message = lines.Length > 1 ? lines[1].Trim() : null;
+    return new ChessMoveResponse(isValid, message, null);
+});
+
 app.Run();
 
 [Description("Computes the total speed of the Lego Velociraptors in km/h")]
@@ -76,4 +108,18 @@ internal record ChessChatMessage(string SessionId, string Message, string FEN)
 {
     public static implicit operator ChatMessage(ChessChatMessage chessChatMessage) =>
         new ChatMessage(ChatRole.User, $"{chessChatMessage.Message}{Environment.NewLine}Current Board FEN: {chessChatMessage.FEN}");
+}
+
+internal record ChessMoveRequest(string SessionId, string FEN, List<string> PossibleMoves)
+{
+    public static implicit operator ChatMessage(ChessMoveRequest chessMoveRequest) =>
+        new ChatMessage(ChatRole.User, $"{chessMoveRequest.FEN}{Environment.NewLine} What move would you make here as black?{Environment.NewLine}" +
+                                       $"The only possible moves are: '{string.Join(", ", chessMoveRequest.PossibleMoves)}'{Environment.NewLine}" +
+                                       $"If you cannot make a move, please say in the first line 'False' and provide an explanation in the second line.{Environment.NewLine}" +
+                                       $"If you can make a move, please say in the first line 'True' and the move only in the second line.");
+}
+
+internal record ChessMoveResponse(bool Valid, string? Message, string? Move)
+{
+
 }
